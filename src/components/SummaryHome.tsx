06520614be53f6e5e4546589977b6f7dab793c11
Filip Andrejuk <filip_andrejuk@hotmail.com>
2022-10-07 14:54:02 -0400

    Pie chart skeleton for visualization of assignment weighting implemented.

--- src/components/SummaryHome.tsx
+++ src/components/SummaryHome.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import { TextField, Button } from "@mui/material";
+import { SummaryPieChart } from "./SummaryPieChart";
 
 export const SummaryHome = (props: {}) => {
   type Params = {
@@ -71,94 +72,104 @@ export const SummaryHome = (props: {}) => {
     console.log(error);
   }
 
+  if (assignmentsData) {
+    console.log(assignmentsData);
+  }
+
   const [selectedAssignment, setSelectedAssignment] =
     React.useState<string>("");
 
   return (
-    <div className="summaryPanel">
-      <div className="summaryRow">
-        <div className="summaryColumn">
-          <b>Assignment Name</b>
+    <div className="summaryHome">
+      <div className="summaryTable">
+        <div className="summaryRow">
+          <div className="summaryColumn">
+            <b>Assignment Name</b>
+          </div>
+          <div className="summaryColumn">
+            <b>Weight</b>
+          </div>
+          <div className="summaryColumn">
+            <b>Class Average</b>
+          </div>
         </div>
-        <div className="summaryColumn">
-          <b>Weight</b>
-        </div>
-        <div className="summaryColumn">
-          <b>Class Average</b>
-        </div>
+        {data ? (
+          assignmentsData.map((entry: any, index: number) => {
+            if (entry.name) {
+              return (
+                <div
+                  className={
+                    selectedAssignment === entry.name
+                      ? "summaryRow summarySelected"
+                      : "summaryRow"
+                  }
+                  key={entry.name}
+                  onClick={() => setSelectedAssignment(entry.name)}
+                >
+                  <div className="summaryColumn">{entry.name}</div>
+                  <div className="summaryColumn">
+                    <TextField
+                      onClick={(e) => e.stopPropagation()}
+                      InputProps={{
+                        inputProps: {
+                          style: { textAlign: "center" },
+                        },
+                      }}
+                      id={`${index}`}
+                      value={entry.weight ? entry.weight : ""}
+                      onChange={(event) => {
+                        setAssignmentsData((prev: any) =>
+                          prev.map((dataPoint: any, index: number) => {
+                            if (isNaN(parseInt(event.target.value))) {
+                              return {
+                                ...dataPoint,
+                                weight: null,
+                              };
+                            }
+                            if (index !== parseInt(event.target.id)) {
+                              return dataPoint;
+                            } else {
+                              return {
+                                ...dataPoint,
+                                weight: parseInt(event.target.value),
+                              };
+                            }
+                          })
+                        );
+                      }}
+                    ></TextField>
+                  </div>
+                  <div className="summaryColumn">
+                    {(
+                      (data.marksData[index].average /
+                        parseInt(data.marksData[index].outof)) *
+                      100
+                    ).toPrecision(3)}
+                    %
+                  </div>
+                </div>
+              );
+            }
+          })
+        ) : (
+          <h1>{error ? "There was an error." : "Loading..."}</h1>
+        )}
+        {!error && (
+          <div className="editMarksAndStudentDetailsButtonContainer">
+            <Button
+              variant="outlined"
+              onClick={() => {
+                submitWeightingChanges(assignmentsData);
+              }}
+            >
+              Submit Weighting Adjustments
+            </Button>
+          </div>
+        )}
       </div>
-      {data ? (
-        assignmentsData.map((entry: any, index: number) => {
-          if (entry.name) {
-            return (
-              <div
-                className={
-                  selectedAssignment === entry.name
-                    ? "summaryRow summarySelected"
-                    : "summaryRow"
-                }
-                key={entry.name}
-                onClick={() => setSelectedAssignment(entry.name)}
-              >
-                <div className="summaryColumn">{entry.name}</div>
-                <div className="summaryColumn">
-                  <TextField
-                    InputProps={{
-                      inputProps: {
-                        style: { textAlign: "center" },
-                      },
-                    }}
-                    id={`${index}`}
-                    value={entry.weight ? entry.weight : ""}
-                    onChange={(event) => {
-                      setAssignmentsData((prev: any) =>
-                        prev.map((dataPoint: any, index: number) => {
-                          if (isNaN(parseInt(event.target.value))) {
-                            return {
-                              ...dataPoint,
-                              weight: null,
-                            };
-                          }
-                          if (index !== parseInt(event.target.id)) {
-                            return dataPoint;
-                          } else {
-                            return {
-                              ...dataPoint,
-                              weight: parseInt(event.target.value),
-                            };
-                          }
-                        })
-                      );
-                    }}
-                  ></TextField>
-                </div>
-                <div className="summaryColumn">
-                  {(
-                    (data.marksData[index].average /
-                      parseInt(data.marksData[index].outof)) *
-                    100
-                  ).toPrecision(3)}
-                  %
-                </div>
-              </div>
-            );
-          }
-        })
-      ) : (
-        <h1>{error ? "There was an error." : "Loading..."}</h1>
-      )}
-      {!error && (
-        <div className="editMarksAndStudentDetailsButtonContainer">
-          <Button
-            variant="outlined"
-            onClick={() => {
-              submitWeightingChanges(assignmentsData);
-            }}
-          >
-            Submit Weighting Adjustments
-          </Button>
-        </div>
-      )}
+      {/* {selectedAssignment ? (
+        <SummaryPieChart assignmentArray={assignmentsData}></SummaryPieChart>
+      ) : null} */}
     </div>
   );
 };
